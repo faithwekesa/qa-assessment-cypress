193e5c36ec14d48ebab5073eabe4220bc964a3e7
faith wekesa <faithwekesanas@gmail.com>
2026-05-19 08:46:26 +0300

    created report

---
+++ Backend/functions-assignment/report_generator.py
@@ -0,0 +1,37 @@
+def report_dict(name, backend, frontend, design, average, grade):
+   
+    return {  "name": name,  "Backend": backend,  "Frontend": frontend,  "Design": design, "average": int(average),  "grade": grade }
+
+
+def generate_report(name, backend, frontend, design):
+  
+    average = (backend + frontend + design) / 3
+
+    # Determine the grade
+    if average >= 80:
+        grade = 'A'
+    elif average >= 70:
+        grade = 'B'
+    elif average >= 60:
+        grade = 'C'
+    elif average >= 50:
+        grade = 'D'
+    else:
+        grade = 'E'
+    
+   
+    return report_dict(name, backend, frontend, design, average, grade)
+
+name = input("Enter student name: ")
+backend = int(input("Enter Backend marks: "))
+frontend = int(input("Enter Frontend marks: "))
+design = int(input("Enter Design marks: "))
+
+
+report = generate_report(name, backend, frontend, design)
+
+
+print("\nOutput:")
+print(report)
+        
+
